95afa6dacd71a4d0e241557547267ef4d39502fc
Mate Boer <37080510+bmz1@users.noreply.github.com>
2026-04-12 12:59:41 +0200

    fix chat followups (#1)

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/index.ts"],
 	format: ["esm", "cjs"],
-	dts: { compilerOptions: { moduleResolution: "bundler" } },
+	dts: { compilerOptions: { moduleResolution: "bundler", incremental: false } },
 	sourcemap: true,
 	clean: true,
 	target: "es2022",
